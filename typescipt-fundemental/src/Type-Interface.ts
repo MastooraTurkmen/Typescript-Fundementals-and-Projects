9c0fe62cbf63df257b22eff4f62d98b1b957b924
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-25 16:00:09 +0430

    first commit

---
+++ typescipt-fundemental/src/Type-Interface.ts
@@ -0,0 +1,24 @@
+let awesomeName: string = "Muhammad";
+awesomeName = "something";
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+let amount: number = 23;
+amount = 2 - 1;
+console.log(amount);
+
+let isAwesome: boolean = true;
+isAwesome = false;
+
+// Challenge
+
+let someValue: string = "something";
+someValue = someValue.toUpperCase();
+console.log(someValue);
+
+let someNumber: number = 4;
+someNumber = someNumber + 30;
+console.log(someNumber);
+
+let someBoolean: boolean = someNumber < 3;
+console.log(someBoolean);
