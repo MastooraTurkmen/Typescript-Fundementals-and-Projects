1b7997aca6b146295347e77af9942f5c22e9bfa3
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-26 21:34:18 +0430

    create type arrays examples and challenges

---
+++ typescipt-fundemental/src/Type-Arrays.ts
@@ -0,0 +1,20 @@
+let prices: number[] = [100, 23, 43];
+prices.pop();
+
+let someFruits: string[] = ["apple", "orange"];
+console.log(someFruits);
+
+let names = ["peter", "susan"];
+console.log(names);
+
+let array: (string | boolean)[] = ["apple", true];
+
+// Challenge
+
+let temperatures: number[] = [20, 32, 54];
+// temperatures.push("Hello");
+
+let colors: string[] = ["red", "blue", "yellow"];
+colors.push("hello");
+
+let mixedArray: (number | string)[] = [1, "two", 33];
