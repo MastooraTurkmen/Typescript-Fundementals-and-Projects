178e7e766be88f78a619628945c5538e33b0394b
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-28 13:52:19 +0430

    type interfaces

--- typescipt-fundemental/src/Type-Interfaces.ts
+++ typescipt-fundemental/src/Type-Interfaces.ts
@@ -76,3 +76,119 @@ const laptop: Computer = {
 
 console.log(laptop);
 console.log(macBook);
+
+// Merge and Extend Interfaces
+
+interface Person {
+  name: string;
+  getDetails(): string;
+}
+
+interface DogOwner {
+  dogName: string;
+  getDogDetails(): string;
+}
+
+interface Person {
+  age: number;
+}
+
+const person: Person = {
+  name: "john",
+  age: 30,
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  },
+};
+
+interface Employee extends Person {
+  employeeId: number;
+}
+
+const employee: Employee = {
+  name: "jane",
+  age: 24,
+  employeeId: 123,
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Employee ID: ${this.employeeId}`;
+  },
+};
+
+console.log(employee.getDetails());
+
+interface Manager extends Person, DogOwner {
+  managePeople(): void;
+}
+
+const manager: Manager = {
+  name: "bob",
+  age: 23,
+  dogName: "buddy",
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  },
+  getDogDetails() {
+    return `Dog Name: ${this.dogName}`;
+  },
+  managePeople() {
+    console.log("I manage people");
+  },
+};
+
+manager.managePeople();
+
+// Challenges
+
+interface Person1 {
+  name: string;
+}
+
+interface DogOwner1 extends Person1 {
+  dogName: string;
+}
+
+interface Manager1 extends Person1 {
+  mangePeople(): void;
+  delegateTasks(): void;
+}
+
+function getEmployee(): Person1 | DogOwner1 | Manager1 {
+  let randomNumber = Math.random();
+
+  if (randomNumber > 0.33) {
+    return {
+      name: "john",
+    };
+  } else if (randomNumber > 0.66) {
+    return {
+      name: "jane",
+      dogName: "buddy",
+    };
+  } else {
+    return {
+      name: "bob",
+      mangePeople() {
+        console.log("I manage people");
+      },
+      delegateTasks() {
+        console.log("I delegate tasks");
+      },
+    };
+  }
+}
+
+const employee1: Person1 | DogOwner1 | Manager1 = getEmployee();
+console.log(employee1);
+
+// Type Predicate
+
+function isManager(obj: Person1 | DogOwner1 | Manager1): obj is Manager1 {
+  // return typeof obj === "object";
+  return "managePeople" in obj;
+}
+
+console.log(isManager(employee1));
+
+if (isManager(employee1)) {
+  employee1.delegateTasks();
+}
