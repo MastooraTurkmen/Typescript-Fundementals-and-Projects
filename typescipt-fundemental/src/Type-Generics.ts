036707cbbf90c3bfd77e78d47d99269248818c10
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-02 08:08:00 +0430

    type generics

---
+++ typescipt-fundemental/src/Type-Generics.ts
@@ -0,0 +1,5 @@
+let array1: string[] = ["Apple", "Banana", "Mango"];
+let array2: number[] = [2, 5, 4];
+let array3: boolean[] = [true, false, true];
+
+let arrays: Array<string> = ["Apple", "Banana", "Mango"];
