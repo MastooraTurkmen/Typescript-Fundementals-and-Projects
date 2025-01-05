6e80e6aec6f3c8c2b1deb753ff42ba287f252093
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-05 15:54:05 +0430

    set up

--- typescript-react-essentials/src/03-state/index.tsx
+++ typescript-react-essentials/src/03-state/index.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 function Component() {
+  const [click, setClick] = useState("He");
+  const [number, setNumber] = useState(3);
+  const [list, setList] = useState<string[]>([]);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      <button
+        className="btn btn-center"
+        onClick={() => {
+          // setClick("typescript");
+          setNumber(32);
+          // setNumber("dd");
+          setList(["dd", "aa"]);
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 }
