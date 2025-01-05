ba517b2bf129753d11bdc672034904a441a2e5d4
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-05 15:54:03 +0430

    set up

--- typescript-react-essentials/src/02-props/index.tsx
+++ typescript-react-essentials/src/02-props/index.tsx
@@ -1,8 +1,13 @@
-function Component() {
+import { type PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{ name: string; id: number }>;
+
+function Component({ name, id, children }: Props) {
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+      <h2>Name: {name}</h2>
+      <h2>Id: {id}</h2>
+      <h2>{children}</h2>
     </div>
   );
 }
