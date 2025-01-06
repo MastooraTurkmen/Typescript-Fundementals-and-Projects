309ead87aee1666b437c6cda725d0e59aec4fa59
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-06 21:37:49 +0430

    set up

--- typescript-react-essentials/src/App.tsx
+++ typescript-react-essentials/src/App.tsx
@@ -1,10 +1,10 @@
 // import Component from "./01-return";
 // import Component from "./02-props";
-import Component from "./03-state";
+// import Component from "./03-state";
 // import Component from "./04-events";
 // import Component from "./05-challenge";
 // import Component from "./06-context";
-// import Component from "./07-reducers";
+import Component from "./07-reducers";
 // import Component from "./08-fetch-data";
 // import Component from "./09-rtk";
 // import Component from "./10-tasks";
@@ -17,6 +17,11 @@ function App() {
     //   <Component name="john" id={2}>
     //     <h1>Hello</h1>
     //   </Component>
+    // </main>
+    // Challenge
+    // <main>
+    //   <Component type="basic" name="susan" />
+    //   <Component type="advanced" name="john" email="john@gmail.com" />
     // </main>
     <main>
       <Component />
