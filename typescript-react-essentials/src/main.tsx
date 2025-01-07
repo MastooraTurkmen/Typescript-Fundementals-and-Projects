920ea65bdb2b2864899f4ca75c0c3512e40a284b
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-07 20:40:23 +0430

    main

--- typescript-react-essentials/src/main.tsx
+++ typescript-react-essentials/src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { store } from "./store.ts";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
+  // <QueryClientProvider client={queryClient}>
+  //   <App />
+  // </QueryClientProvider>
+  <Provider store={store}>
     <App />
-  </QueryClientProvider>
+  </Provider>
 );
